415dab537c8bb4f2d6af2b32e97a5b9b418f0a6b
Marwin Baumann <mbaumann@schubergphilis.com>
2025-04-11 15:29:11 +0200

    add moved

--- moved.tf
+++ moved.tf
@@ -2,3 +2,8 @@ moved {
   from = aws_iam_openid_connect_provider.default["instance"]
   to   = aws_iam_openid_connect_provider.default["create"]
 }
+
+moved {
+  from = data.tls_certificate.default
+  to   = data.tls_certificate.default["create"]
+}
